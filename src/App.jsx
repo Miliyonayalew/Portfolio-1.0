86380293afd76a0598d83a567ce14ddff5dc4788
Miliyonayalew <miliayalew@gmail.com>
2023-04-04 00:13:56 +0300

    Add image and cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import './App.css'
+import React from 'react'
+import Navbar from './Components/Navbar'
+import About from './Components/About'
+import Hero from './Components/Hero'
+import Project from './Components/Project'
+import Skills from './Components/Skills'
+import Contact from './Components/Contact'
+import Footer from './Components/Footer'
 
-function App() {
-
-
+const App = () => {
   return (
-    <div className="App">
-     <p>Hello</p>
-      
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Project />
+      <Contact />
+      <Footer />
     </div>
   )
 }
 
-export default App
+export default App;
