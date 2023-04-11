a637a3c4677621f1dde44a8e861fdf3c3a1a7b5d
Miliyonayalew <miliayalew@gmail.com>
2023-04-11 17:46:22 +0300

    followed best practice for Hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import hero from '../assets/images/hero.webp';
 
 const Hero = () => {
-  const social_media = [
+  const socialMedia = [
     {
       name: 'Github',
       icon: 'logo-github',
@@ -25,7 +25,7 @@ const Hero = () => {
       className="min-h-screen flex py-10 md:flex-row flex-col items-center"
     >
       <div className="flex-1 flex items-center justify-center h-full">
-        <img src={hero} alt="hero" fetchpriority="high" className="md:w-10/11 h-full object-cover" />
+        <img src={hero} alt="hero" className="md:w-10/11 h-full object-cover" />
       </div>
       <div className="flex-1">
         <div className="md:text-left text-center">
@@ -34,7 +34,7 @@ const Hero = () => {
               Hello!
               <br />
             </span>
-            I'm
+            I&apos;m
             {' '}
             <span>Miliyon Ayalew</span>
           </h1>
@@ -45,12 +45,12 @@ const Hero = () => {
             See my work and skills - I can help you build your dream product, feature or website.
           </p>
           <a href="#contact">
-            <button className="btn-primary mt-8">Contact Me</button>
+            <button type="button" className="btn-primary mt-8">Contact Me</button>
           </a>
           <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
-            {social_media?.map((icon, i) => (
+            {socialMedia?.map((icon) => (
               <div
-                key={i}
+                key={icon.name}
                 className="text-gray-600 hover:text-white cursor-pointer "
               >
                 <a href={icon.link} alt={icon.name} target="_blank" rel="noreferrer">
